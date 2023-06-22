a6dd5695f50b7e93400400b67cd74a1982c47ba4
Angela Shen <angelayshen@gmail.com>
2023-06-22 15:29:19 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    num = 0
+    for num in range(len(dwarves)):
+        print(f'{num + 1}. {dwarves[num]}')
+        num += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f'{planeteer.capitalize()}!' for planeteer in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in cheeses:
+        if cheese in items:
+            return cheese
+    return None
--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,16 @@ from cartoon_collections import (
 )
 
 if __name__ == '__main__':
+
+    dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+    
+    planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+
+    short_words = ["puff", "go", "two"]
+    assorted_words = ["two", "go", "industrious", "bop"]
+
+    snacks = ["crackers", "gouda", "thyme"]
+    soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+    ingredients = ["garlic", "rosemary", "bread"]
+
     import ipdb; ipdb.set_trace()
